1003d1299c0b8bed365dcd44ac463a222a91fade
Ricky McCallum <rickymccallum87@gmail.com>
2018-05-29 16:51:45 -0400

    palindromes() first attempt, non-working

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,25 @@
-var palindromes = function() {
+var palindromes = function(str) {
+	// Ignore case
+	str = str.toLowerCase();
 
+	console.log(str);
+	// Ignore non-alpha
+	let arr = str.split('');
+	for (let i = 0; i < arr.length; i++) {
+		if (!arr[i].isAlpha) {
+			arr.slice(i, 1);
+		}
+	}
+	console.log(arr.join(''));
+
+	// Check for palindrome
+	while (arr.length > 1) {
+		if (arr.pop() !== arr.shift()) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
 module.exports = palindromes
