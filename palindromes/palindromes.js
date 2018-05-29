35cfc67e85b37d699d32da341a6cfcdd842f5439
Ricky McCallum <rickymccallum87@gmail.com>
2018-05-29 17:26:26 -0400

    palindromes() passes all tests

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -2,15 +2,18 @@ var palindromes = function(str) {
 	// Ignore case
 	str = str.toLowerCase();
 
-	console.log(str);
 	// Ignore non-alpha
 	let arr = str.split('');
+	let alpha = /^[a-z]+$/;
 	for (let i = 0; i < arr.length; i++) {
-		if (!arr[i].isAlpha) {
-			arr.slice(i, 1);
+
+		// Remove punctuation
+		if (!arr[i].match(alpha)) {
+			arr.splice(i, 1);
+			i--;
 		}
+
 	}
-	console.log(arr.join(''));
 
 	// Check for palindrome
 	while (arr.length > 1) {
