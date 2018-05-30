6f91d47b584ae712e682360409d0e6f366e6b7a8
Ricky McCallum <rickymccallum87@gmail.com>
2018-05-29 21:18:35 -0400

    detect WTF case

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,18 @@
-var snakeCase = function() {
-
+var snakeCase = function(str) {
+	console.log('\n');
+	console.log(str);
+	// Detect WTF case
+	wtf = /^[a-z]+\.\.[a-z]/i
+	if (str.match(wtf)) {
+		console.log('wtf case');
+	}
+	
+	// Detect kebab case
+	
+	// Detect camel case
+	
+	// Detect spaced phrases
+	console.log('\n');
 }
 
 module.exports = snakeCase
