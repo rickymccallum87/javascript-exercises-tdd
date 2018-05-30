f53e3fdd8bdf7335cfd10820bb17d455d59eb82a
Ricky McCallum <rickymccallum87@gmail.com>
2018-05-30 09:04:51 -0400

    snakeCase() can identify each type of case passed in

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -2,16 +2,30 @@ var snakeCase = function(str) {
 	console.log('\n');
 	console.log(str);
 	// Detect WTF case
-	wtf = /^[a-z]+\.\.[a-z]/i
+	const wtf = /^[a-z]+\.\.[a-z]+/i;
 	if (str.match(wtf)) {
 		console.log('wtf case');
+		return;
 	}
 	
 	// Detect kebab case
+	const kebab = /^[a-z]+-[a-z]+/i;
+	if (str.match(kebab)) {
+		console.log('kebab case');
+		return;
+	}
 	
 	// Detect camel case
+	const camel = /^[a-z]+[A-Z][a-z]+/;
+	if (str.match(camel)) {
+		console.log('camel case');
+		return;
+	}
 	
 	// Detect spaced phrases
+	let arr = str.split(' ');
+	console.log(arr);
+
 	console.log('\n');
 }
 
