65d9a3580b2521f256016f2eef85399d6dc8b6f3
Ricky McCallum <rickymccallum87@gmail.com>
2018-05-31 08:43:23 -0400

    first attempt at caesar()

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,16 @@
-var caesar = function() {
+var caesar = function(str, shift) {
+	// A: 65, Z: 90, a: 97, z: 122
+	console.log('\n');
+	let newStr = '';
+	for (let i = 0; i < str.length; i++) {
+		console.log(str.charCodeAt(i));
+		newStr += String.fromCodePoint(str.charCodeAt(i) + shift);
+	}
+	
+	console.log(str);
+	return newStr;
 
+	console.log('\n');
 }
 
 module.exports = caesar
