b417571f47063a089d5721e2a3e5bc8c302f7a52
Ricky McCallum <rickymccallum87@gmail.com>
2018-06-01 12:38:35 -0400

    translate() works with single words and start sounds made with multiple consonants

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,11 @@
-function translate() {
-	// body...
+function translate(str) {
+	if (str[0].match(/[aeiou]/)) { // first letter is a vowel
+		return str.concat('ay');
+	} else {
+		startSound = str.match(/[^aeiou]+/);
+		console.log(startSound, startSound.length);
+		return str.slice(startSound[0].length).concat(startSound[0] + 'ay');
+	}
 }
 
 
