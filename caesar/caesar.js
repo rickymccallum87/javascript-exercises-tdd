7a1d5c390928ed03baa84ca8d6a632a4daad3917
Ricky McCallum <rickymccallum87@gmail.com>
2018-06-01 11:29:27 -0400

    caesar() passes all tests

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,4 +1,9 @@
 var caesar = function(str, shift) {
+	// Account for large shift factors
+	while (shift > 26) {
+		shift -= 26;
+	}
+
 	// A: 65, Z: 90, a: 97, z: 122
 	let oldChar, oldCharCode;
 	let newChar, newCharCode;
@@ -16,11 +21,7 @@ var caesar = function(str, shift) {
 
 			// Wrap around alphabet
 			newCharCode = str.charCodeAt(i) + shift;
-			if (oldCharCode > 96 && newCharCode > 122) { // Lowercase
-				newCharCode -= 26;
-			} else if (oldCharCode < 91 && newCharCode > 90) { // Capitals
-				newCharCode -= 26;
-			}
+			newCharCode = wrapCheck(oldCharCode, newCharCode);
 
 			newChar = String.fromCodePoint(newCharCode);
 			newStr += newChar;
@@ -30,4 +31,18 @@ var caesar = function(str, shift) {
 	return newStr;
 }
 
+function wrapCheck(oldCharCode, newCharCode) {
+	if (oldCharCode >= 97 && newCharCode > 122) { // Lowercase past z
+		newCharCode -= 26;
+	} else if (oldCharCode >= 97 && newCharCode < 97) { // Lowercase past a
+		newCharCode += 26;
+	} else if (oldCharCode <= 90 && newCharCode > 90) { // Capitals past Z
+		newCharCode -= 26;
+	} else if (oldCharCode <= 90 && newCharCode < 65) { // Capitals past A
+		newCharCode += 26;
+	}
+
+	return newCharCode;
+
+}
 module.exports = caesar
