cf5fe2d508a5a6a63810f9f3316c10c04eeff501
Ricky McCallum <rickymccallum87@gmail.com>
2018-06-01 12:08:41 -0400

    fibonacci() passes all tests

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-var fibonacci = function() {
+var fibonacci = function(num) {
+	// Do not accept negatives
+	if (num < 0) {
+		return 'OOPS';
+	}
 
+	// Accept strings
+	num = +num;
+
+	let fib = [1,1]
+	for (let i = 2; i < num; i++) {
+		fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+	}
+	return fib.pop();
 }
 
 module.exports = fibonacci
