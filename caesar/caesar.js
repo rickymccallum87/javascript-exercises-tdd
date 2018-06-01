7b4e4f64c84ccfad952edcf9308ca970d47ebc21
Ricky McCallum <rickymccallum87@gmail.com>
2018-06-01 09:55:38 -0400

    caesar() works with words and phrases, wraps correctly

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,16 +1,33 @@
 var caesar = function(str, shift) {
 	// A: 65, Z: 90, a: 97, z: 122
-	console.log('\n');
+	let oldChar, oldCharCode;
+	let newChar, newCharCode;
 	let newStr = '';
-	for (let i = 0; i < str.length; i++) {
-		console.log(str.charCodeAt(i));
-		newStr += String.fromCodePoint(str.charCodeAt(i) + shift);
-	}
-	
-	console.log(str);
-	return newStr;
+	let punct = [' ', ',', '.', '!', '?']
 
-	console.log('\n');
+	for (let i = 0; i < str.length; i++) {
+		oldChar = str[i];
+		oldCharCode = str.charCodeAt(i);
+
+		// Ignore punctuation
+		if (punct.includes(oldChar)) {
+			newStr += oldChar;
+		} else {
+
+			// Wrap around alphabet
+			newCharCode = str.charCodeAt(i) + shift;
+			if (oldCharCode > 96 && newCharCode > 122) { // Lowercase
+				newCharCode -= 26;
+			} else if (oldCharCode < 91 && newCharCode > 90) { // Capitals
+				newCharCode -= 26;
+			}
+
+			newChar = String.fromCodePoint(newCharCode);
+			newStr += newChar;
+		}
+	}
+
+	return newStr;
 }
 
 module.exports = caesar
